f358a901078f070db4f17eaa8f53a169851b96ff
Gott50 <xxGott50xx@gmail.com>
2016-09-10 21:46:10 +0200

    update generateWorkout(duration, location)

--- index.js
+++ index.js
@@ -60,9 +60,9 @@ restService.listen((process.env.PORT || 5000), function () {
 });
 
 
-var duration,location;
+var duration = 0,location = "";
 function generateWorkout(duration, location) {
-    this.duration = duration.amount <= 60? duration.amount : 60;
+    this.duration = duration && duration.amount <= 60? duration.amount : 60;
     this.location = location;
 
     return workouts.filter(isInLocation).filter(isInDuration).map(printWorkout).join("\n");
