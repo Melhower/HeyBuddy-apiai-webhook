eccc92dbfe1d945e4b6273232f6173fec4bdc0e8
Gott50 <xxGott50xx@gmail.com>
2017-01-07 22:13:43 +0100

    fix Can't process request ReferenceError: generateWorkout is not defined

--- index.js
+++ index.js
@@ -12,65 +12,6 @@ restService.use(bodyParser.json());
 var thisSessionId;
 
 restService.post('/hook', function (req, res) {
-
-    console.log('hook request');
-
-    console.log("request: "+JSON.stringify(req.body));
-
-    try {
-        var speech = 'empty speech';
-        var data = 'empty data';
-
-        if (req.body) {
-            var requestBody = req.body;
-
-            if (requestBody.result) {
-                speech = '';
-                data = {};
-
-                if (requestBody.sessionId) {
-                    thisSessionId = requestBody.sessionId;
-                }
-
-                if (requestBody.result.fulfillment) {
-                    speech += requestBody.result.fulfillment.speech;
-                    speech += ' ';
-                }
-
-                if (requestBody.result.action === "generateWorkout") {
-                    let generateWorkout = generateWorkout(parsDuration(requestBody.result.parameters.duration), parsLocation(requestBody.result.parameters.location));
-                    if (generateWorkout)
-                    data = {
-                        distributor: generateWorkout.messages,
-                    };
-
-                }
-
-                else if (requestBody.result.action) 
-                    speech += 'action: ' + requestBody.result.action;
-            }
-        }
-
-        console.log('result: ', speech);
-
-        return res.json({
-            speech: speech,
-            displayText: speech,
-            data: data,
-            source: 'apiai-webhook-sample'
-        });
-    } catch (err) {
-        console.error("Can't process request", err);
-
-        return res.status(400).json({
-            status: {
-                code: 400,
-                errorType: err.message
-            }
-        });
-    }
-
-
     function parsDuration(duration){
         console.log("duration: "+JSON.stringify(duration));
         if (duration == "") { return 30}
@@ -98,9 +39,67 @@ restService.post('/hook', function (req, res) {
     function isInDuration(workout, duration) {
         return duration - workout.duration >= 0 && duration - workout.duration < 5;
     }
+
+
+    console.log('hook request');
+
+    console.log("request: "+JSON.stringify(req.body));
+
+    try {
+        var speech = 'empty speech';
+        var data = 'empty data';
+
+        if (req.body) {
+            var requestBody = req.body;
+
+            if (requestBody.result) {
+                speech = '';
+                data = {};
+
+                if (requestBody.sessionId) {
+                    thisSessionId = requestBody.sessionId;
+                }
+
+                if (requestBody.result.fulfillment) {
+                    speech += requestBody.result.fulfillment.speech;
+                    speech += ' ';
+                }
+
+                if (requestBody.result.action === "generateWorkout") {
+                    let out = generateWorkout(parsDuration(requestBody.result.parameters.duration), parsLocation(requestBody.result.parameters.location))[0];
+                    if (out)
+                    data = {
+                        distributor: out.messages,
+                    };
+                }
+
+                else if (requestBody.result.action) 
+                    speech += 'action: ' + requestBody.result.action;
+            }
+        }
+
+        console.log('speech: ', speech);
+        console.log('data: ', data);
+
+        return res.json({
+            speech: speech,
+            displayText: speech,
+            data: data,
+            source: 'apiai-webhook-sample'
+        });
+    } catch (err) {
+        console.error("Can't process request", err);
+
+        return res.status(400).json({
+            status: {
+                code: 400,
+                errorType: err.message
+            }
+        });
+    }
+
 });
 
 restService.listen((process.env.PORT || 5000), function () {
     console.log("Server listening");
 });
-
