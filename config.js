b7c137d4858d3b5393e42fd40a94927474bffe2b
Gott50 <xxGott50xx@gmail.com>
2017-01-07 20:28:40 +0100

    add RichResponse

---
+++ config.js
@@ -0,0 +1,45 @@
+module.exports = [
+    {
+        locations: "home",
+        duration: 30,
+        "messages": [
+            {
+                "type": 0,
+                "speech": "Here's to your fitness:"
+            },
+            {
+                "imageUrl": "https://github.com/Push2Aim/JettyChatBot/blob/master/src/main/webapp/What%20u%20need%20Gym%2015+.png?raw=true",
+                "type": 3
+            },
+            {
+                "type": 0,
+                "speech": "You are going to do ① a warmup, ② an AMRAP workout.\nWhat's AMRAP? Below's an example as explanation⬇️"
+            },
+            {
+                "imageUrl": "https://github.com/Push2Aim/JettyChatBot/blob/master/src/main/webapp/Amrap%20tutorial.png?raw=true",
+                "type": 3
+            },
+            {
+                "type": 0,
+                "speech": "①, your 10 min warmup⬇️\n2 minutes Rower\n 15 Burpees\nStretching with this pic:"
+            },
+            {
+                "imageUrl": "https://github.com/Push2Aim/JettyChatBot/blob/master/src/main/webapp/Stretching%206,5%20min.png?raw=true",
+                "type": 3
+            },
+            {
+                "type": 0,
+                "speech": "Now, your AMRAP⬇️\n② 20 minutes AMRAP \n20 Mountain Climbers (per leg)\n15 Bench Presses\n12 Squats\n9 Dumbbell Shoulder Presses\n7 Leg Raises\n5 Squat Jumps"
+            },
+            {
+                "title": "In case...:",
+                "replies": [
+                    "what's AMRAP???",
+                    "outdoor workout",
+                    "home workout"
+                ],
+                "type": 2
+            }
+        ]
+    },
+];
