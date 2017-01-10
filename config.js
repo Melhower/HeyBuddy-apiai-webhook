224159ca020b5ff26b2b1910a4116c0667a54ebb
burdaproject <burdaproject@gmail.com>
2017-01-10 17:29:13 +0100

    Updated 30 min home

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ module.exports = [
                 "rest": 1000
             },
             {
-                "imageUrl": "https://github.com/Push2Aim/JettyChatBot/blob/master/src/main/webapp/Stretching%206,5%20min.png?raw=true",
+                "imageUrl": "https://github.com/Push2Aim/JettyChatBot/blob/master/src/main/webapp/warmup%2030%20min%20workout.png?raw=true",
                 "type": 3
             },
             {
